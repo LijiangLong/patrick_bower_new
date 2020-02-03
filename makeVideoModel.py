d7503bc39b9a0ae2a9f2a88037d323c383e12ac3
Lijiang Long <llong35@gatech.edu>
2020-02-03 04:32:46 -0500

    debug

--- makeVideoModel.py
+++ makeVideoModel.py
@@ -1,9 +1,13 @@
 from Modules.FileManagers.FileManager import FileManager as FM
 import subprocess
+import pdb
+
 
 fm_obj = FM()
+a = fm_obj.localOrganizedLabeledClipsDir
+b = fm_obj.local3DVideosDir
 #fm_obj.downloadAnnotationData('LabeledVideos')
 
-subprocess.run(['python3', 'Modules/MachineLearning/3D_resnet.py', '--data', fm_obj.localOrganizedLabeledClipsDir, '--results', fm_obj.local3DVideosDir])
+#subprocess.run(['python3', 'Modules/MachineLearning/3D_resnet.py', '--data', fm_obj.localOrganizedLabeledClipsDir, '--results', fm_obj.local3DVideosDir])
 
 
